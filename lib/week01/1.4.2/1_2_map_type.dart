dc77733b96cc5400346f2b57933894ff724d9aa6
Joonsu Yoon <bananamilk452@gmail.com>
2025-09-07 12:58:59 +0900

    feat(week01): Print map keys and values in 1_2_map_type.dart
    Replaces printing specific map entries with printing all keys and values.

--- lib/week01/1.4.2/1_2_map_type.dart
+++ lib/week01/1.4.2/1_2_map_type.dart
@@ -4,6 +4,8 @@ void main() {
     'Ron Weasley': '론 위즐리',
     'Hermione Granger': '헤르미온느 그레인저',
   };
-  print(dictionary['Harry Potter']);
-  print(dictionary['Hermione Granger']);
+
+  print(dictionary.keys);
+  // Iterable이 반환되기 때문에 .toList()를 실행해서 List를 반환받을 수도 있음
+  print(dictionary.values);
 }
